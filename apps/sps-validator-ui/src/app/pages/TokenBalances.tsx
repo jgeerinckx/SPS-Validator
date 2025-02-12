0c27aaa1e0cf5ba8237b01814fc5977b3e48c8ce
Jef <jgeerinckx@hotmail.com>
2025-02-12 18:48:56 +0100

    Added dark mode to TokenBalences
    Added dark mode to TokenBalences page

--- apps/sps-validator-ui/src/app/pages/TokenBalances.tsx
+++ apps/sps-validator-ui/src/app/pages/TokenBalances.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { DefaultService } from '../services/openapi';
 import { usePromise } from '../hooks/Promise';
 import { Table, TableBody, TableCell, TableColumn, TableHead, TablePager, TableRow } from '../components/Table';
+import useSpinnerColor from '../hooks/SpinnerColor'
 
 const tokens = ['SPS', 'SPSP', 'LICENSE'];
 
@@ -12,6 +13,7 @@ function TokenBalancesTab({ token }: { token: string }) {
     const [systemAccounts, setSystemAccounts] = useState(false);
     const [count, isLoadingCount] = usePromise(() => DefaultService.getBalancesByToken(token, 0, 0, systemAccounts), [token, systemAccounts]);
     const [balances, isLoading] = usePromise(() => DefaultService.getBalancesByToken(token, limit, page * limit, systemAccounts), [token, page, limit, systemAccounts]);
+    const spinnerColor = useSpinnerColor("blue")
 
     const toggleSystemAccounts = (value: boolean) => {
         setSystemAccounts(value);
@@ -19,29 +21,29 @@ function TokenBalancesTab({ token }: { token: string }) {
     };
 
     if (isLoading || isLoadingCount) {
-        return <Spinner className="w-full" />;
+        return <Spinner className="w-full" color={spinnerColor}/>;
     }
 
     return (
         <>
-            <Table className="w-full border-2 border-gray-200">
+            <Table className="w-full border-2 border-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-300">
                 <TableHead>
                     <TableRow>
-                        <TableColumn>Token</TableColumn>
-                        <TableColumn>
+                        <TableColumn className="dark:bg-gray-300 dark:text-gray-800">Token</TableColumn>
+                        <TableColumn className="dark:bg-gray-300 dark:text-gray-800">
                             <div className="flex items-center">
                                 Player
                                 <div className="ms-5">
-                                    <Checkbox checked={systemAccounts} label="Include System Accounts" onChange={(e) => toggleSystemAccounts(e.target.checked)} />
+                                    <Checkbox checked={systemAccounts} label="Include System Accounts" onChange={(e) => toggleSystemAccounts(e.target.checked)} className="dark:checked:bg-blue-800 dark:border-gray-800 dark:before:bg-blue-400 dark:checked:before:bg-blue-400 dark:text-gray-800" labelProps={{className: "dark:text-gray-800"}}/>
                                 </div>
                             </div>
                         </TableColumn>
-                        <TableColumn>Balance</TableColumn>
+                        <TableColumn className="dark:bg-gray-300 dark:text-gray-800">Balance</TableColumn>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {balances?.balances?.map((balance) => (
-                        <TableRow key={balance.player}>
+                        <TableRow key={balance.player} className="dark:border-gray-300">
                             <TableCell>{token}</TableCell>
                             <TableCell>{balance.player}</TableCell>
                             <TableCell>{balance.balance.toLocaleString()}</TableCell>
@@ -56,15 +58,15 @@ function TokenBalancesTab({ token }: { token: string }) {
 
 export function TokenBalances() {
     return (
-        <Card>
+        <Card className="dark:bg-gray-800 dark:text-gray-300">
             <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
+                <Typography variant="h5" color="blue-gray" className="mb-2 dark:text-gray-200">
                     Token Balances
                 </Typography>
                 <Tabs className="mt-4" value={tokens[0]}>
-                    <TabsHeader>
+                    <TabsHeader className="dark:bg-gray-300 dark:text-gray-800" indicatorProps={{className: "dark:bg-blue-800"}}>
                         {tokens.map((token) => (
-                            <Tab key={token} value={token}>
+                            <Tab key={token} value={token} activeClassName="dark:text-gray-300">
                                 {token}
                             </Tab>
                         ))}
