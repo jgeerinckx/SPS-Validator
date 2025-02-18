64d67186bd35750878271c60d8544d1495ffe46b
Jef <jgeerinckx@hotmail.com>
2025-02-18 02:16:45 +0100

    Update ManageValidatorNode.tsx
    Added dark mode styling to new reward account input

--- apps/sps-validator-ui/src/app/pages/ManageValidatorNode.tsx
+++ apps/sps-validator-ui/src/app/pages/ManageValidatorNode.tsx
@@ -119,6 +119,8 @@ function RegisterCard({ account, registered }: { account: string; registered: ()
                                 value={rewardAccount}
                                 disabled={progress}
                                 onChange={(e) => setRewardAccount(e.target.value.trim())}
+                                className="dark:text-gray-300 dark:focus:border-gray-300 dark:focus:border-t-transparent dark:placeholder:text-gray-300 dark:focus:placeholder:text-gray-500" 
+                                labelProps={{className: "dark:peer-placeholder-shown:text-gray-300 dark:placeholder:text-gray-300 dark:text-gray-300 dark:peer-focus:text-gray-300 dark:peer-focus:before:!border-gray-300 dark:peer-focus:after:!border-gray-300"}}
                             />
                         </div>
                     </form>
@@ -227,6 +229,8 @@ function ManageCard({ account, validator, reloadValidator }: { account: string; 
                                     value={rewardAccount}
                                     disabled={progress}
                                     onChange={(e) => setRewardAccount(e.target.value.trim())}
+                                    className="dark:text-gray-300 dark:focus:border-gray-300 dark:focus:border-t-transparent dark:placeholder:text-gray-300 dark:focus:placeholder:text-gray-500" 
+                                    labelProps={{className: "dark:peer-placeholder-shown:text-gray-300 dark:placeholder:text-gray-300 dark:text-gray-300 dark:peer-focus:text-gray-300 dark:peer-focus:before:!border-gray-300 dark:peer-focus:after:!border-gray-300"}}
                                 />
                             </div>{' '}
                             {!isActive && (
